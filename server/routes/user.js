32d4dcda76d568a01086803596f547bf02e789ab
Luca Schultz <s6lcschu@uni-bonn.de>
2020-06-21 16:27:22 +0200

    Changed demo route to required funtionality

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,13 @@ const database = require("../database.js");
 const userRoutes = (app, fs) => {
 
   app.get("/api/user", (req, res) => {
-    database.read().then((data) => res.send(data));
+    database.read().then(data => {
+      let users = [];
+      data.users.forEach(user => {
+        users.push({ 'user': user.user});
+      });
+      res.send(JSON.stringify(users));
+    });
   });
 
 
