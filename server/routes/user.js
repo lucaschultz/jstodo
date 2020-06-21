d1a23f8f55e39d7fc22b502ca76652ed61eb3c0a
Luca Schultz <s6lcschu@uni-bonn.de>
2020-06-21 15:40:43 +0200

    Demo route to uses async database module now

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,12 @@
+const database = require("../database.js");
+
 const userRoutes = (app, fs) => {
-  // Konfiguration
-  const dataPath = "./database.json";
 
-  // Lese die Daten aus der JSON
   app.get("/api/user", (req, res) => {
-    fs.readFile(dataPath, "utf8", (err, data) => {
-      if (err) {
-        throw err;
-      }
-
-      res.send(JSON.parse(data));
-    });
+    database.read().then((data) => res.send(data));
   });
+
+
 };
 
 module.exports = userRoutes;
