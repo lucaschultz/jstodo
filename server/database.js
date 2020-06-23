f3b5651c9a208a003383960747f8edb5f17cb1af
Luca Schultz <s6lcschu@uni-bonn.de>
2020-06-23 16:19:11 +0200

    Major rewrite of Database methods
    - API of Database class has not changed
    - Introduced helper methods for common actions

--- server/database.js
+++ server/database.js
@@ -103,7 +103,7 @@ function inArray (arr, obj, key) {
   }
 }
 
-function pushIfUnique (arr, obj, key, rename = false) {
+function pushIfUnique (arr, obj, key, type, rename = false) {
   // Teste ob ein Objekt mit dem selben Key existiert 
   const exists = inArray(arr, obj, key)
   // Wenn eines existiert ...
@@ -111,7 +111,7 @@ function pushIfUnique (arr, obj, key, rename = false) {
     // ... und nicht umbennant werden soll ...
     if (!rename) {
       // ... wird das Objekt nicht hinzugefügt.
-      return false;
+      throw new DuplicateError(`${type} with ID '${name}' can't be be added because the ID is already assigned`);
     } else {
       // Wenn umbennant werden soll wird der Key geändert ... 
       obj[key] = incrementName(obj[key]);
@@ -123,10 +123,20 @@ function pushIfUnique (arr, obj, key, rename = false) {
     // ... füge den Objekt hinzu.
     arr.push(obj);  
   }
-  // Gib true zurück falls das Objekt hinzugefügt wurde
-  return true;
 }
 
+function findObjectInArray (arr, key, value) {
+  return arr.findIndex(e => e[key].toLowerCase() === value.toLowerCase());
+}
+
+function deleteObjectInArray (arr, key, value, type) {
+  const filtered = arr.filter(u => u[key].toLowerCase() !== value.toLowerCase());
+  if (equalArraysByID(filtered, arr, key)) {
+    throw new MissingError(`${type} with ID '${name}' can't be deleted because the ID is not assigned`);
+  } else {
+    return filtered;
+  }
+}
 
 class JSONDatabase {
 
@@ -151,6 +161,35 @@ class JSONDatabase {
     await fs.writeFile(path, JSON.stringify(template, null, 2), 'utf8');
   }
 
+  findUser(name) {
+    const index = findObjectInArray(this.data.users, 'user', name);
+    if (index === -1) {
+      throw new MissingError(`User with ID '${name}' can't be found because the ID is not assigned`);
+    } else {
+      return this.data.users[index];
+    }
+  }
+
+  findList(userName, listName) {
+    const user = this.findUser(userName);
+    const index = findObjectInArray(user.lists, 'name', listName);
+    if (index === -1) {
+      throw new MissingError(`List with ID '${listName}' of user '${userName}' can't be found because the ID is not assigned`);
+    } else {
+      return user.lists[index];
+    }
+  }
+
+  findItem(userName, listName, itemName) {
+    const list = this.findList(userName, listName);
+    const index = findObjectInArray(list.items, 'name', itemName);    
+    if (index === -1) {
+      throw new MissingError(`Item with ID '${itemName}' in list '${listName}' of user '${userName}' can't be found because the ID is not assigned`);
+    } else {
+      return list.items[index];
+    }
+  }
+
 
   // Methoden für die api/user Route
 
@@ -163,30 +202,19 @@ class JSONDatabase {
   }
 
   async addUser (name) {
-    const template = { user: name, lists: [] };
-    const pushed = pushIfUnique (this.data.users, template, 'user');
-    if (!pushed) {
-      throw new DuplicateError(`User with ID '${name}' can't be be added because the ID is already assigned`);
-    }
+    const newUser = { user: name, lists: [] };
+    pushIfUnique(this.data.users, newUser, 'user', 'User');
     await this.save();
   }
 
   async renameUser (oldName, newName) {
-    let index = this.data.users.findIndex(u => u.user.toLowerCase() === oldName.toLowerCase());
-    if (index === -1) {
-      throw new MissingError(`User with ID '${oldName}' can't be renamed because the ID is not assigned`);
-    }
-    this.data.users[index].user = newName;
+    user = this.findUser(oldName);
+    user.user = newName;
     await this.save();
   }
 
   async deleteUser (name) {
-    const filtered = this.data.users.filter(u => u.user.toLowerCase() !== name.toLowerCase());
-    if (equalArraysByID(filtered, this.data.users, 'user')) {
-      throw new MissingError(`User with ID '${name}' can't be deleted because the ID is not assigned`);
-    } else {
-      this.data.users = filtered;
-    }
+    this.data.users = deleteObjectInArray(this.data.users, "user", name, "User");
     await this.save();
   }
 
@@ -194,53 +222,45 @@ class JSONDatabase {
   // Methoden für die api/list Route
 
   async getLists(userName) {
-    let lists = [];
-    let index = this.data.users.findIndex(u => u.user.toLowerCase() === userName.toLowerCase());
-    if (index === -1) {
-      throw new MissingError(`Lists user with ID '${userName}' not found because the ID is not assigned`);
-    }
-    this.data.users[index].lists.forEach(list => {
-      lists.push(list);
-    });
-    return lists;
+    return this.findUser(userName).lists;
   }
 
   async addList(userName, listName) {
-    const template = { name: listName, items: [] };
-    let index = this.data.users.findIndex(u => {
-      return u.user.toLowerCase() === userName.toLowerCase()});
-    if (index === -1) {
-      throw new MissingError(`Lists user with ID '${userName}' not found because the ID is not assigned`);
-    }
-    const pushed = pushIfUnique (this.data.users[index].lists, template, 'name');
-    if (!pushed) {
-      throw new DuplicateError(`List of with ID '${listName}' can't be be added to user '${userName}' because the ID is already assigned`);
-    }    
+    const newList = { name: listName, items: [] };
+    const user = this.findUser(username);
+    pushIfUnique(user.lists, newList, 'name', 'List');
     await this.save();
   }
 
   async renameList(userName, oldListName, newListName) {
-    let listIndex = this.data.users[userIndex].lists
-      .findIndex(l => l.name.toLowerCase() === oldListName.toLowerCase());
-    if (listIndex === -1) {
-      throw new MissingError(`List with with ID '${oldListName}' of user '${userName}' not found because the ID is not assigned`);
-    }
-    this.data.users[userIndex].lists[listIndex].name = newListName;
+    const list = this.findList(userName, oldListName);
+    list.name = newListName;
     await this.save();
   }
 
   async deleteList (userName, listName) {
-    let userIndex = this.data.users.findIndex(u => u.user.toLowerCase() === userName.toLowerCase());
-    if (userIndex === -1) {
-      throw new MissingError(`User with ID '${userName}' not found because the ID is not assigned`);
-    }
-    const filtered = this.data.users[userIndex].lists
-      .filter(l => l.name.toLowerCase() !== listName.toLowerCase());
-    if (equalArraysByID(filtered, this.data.users[userIndex].lists, 'user')) {
-      throw new MissingError(`User with ID '${listName}' of user '${userName}' can't be deleted because the ID is not assigned`);
-    } else {
-      this.data.users[userIndex].lists = filtered;
-    }
+    const user = this.findUser(userName);
+    user.lists = deleteObjectInArray(user.lists, "name", listName, "List");
+    await this.save();
+  }
+
+
+  // Methoden für die api/item Route
+
+  async addItem(userName, listName, item) {
+    const list = this.findList(userName, listName);
+    pushIfUnique(list.items, item, 'name', 'Item');
+    await this.save();
+  }
+
+  async updateItem(userName, listName, item) {
+    this.deleteItem(userName, listName, item.name)
+      .then(() => this.addItem(userName, itemName, item));
+  }
+
+  async deleteItem(userName, listName, itemName) {
+    const list = this.findListe(userName, listName);
+    list.items = deleteObjectInArray(list.items, "name", itemName, "Item");
     await this.save();
   }
 }
